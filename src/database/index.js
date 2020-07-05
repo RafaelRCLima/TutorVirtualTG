fca8abddaa611705579edeb7d9e4aebe24379c59
RafaelRCLima <rafael.lima19-92@hotmail.com>
2020-07-05 20:25:31 -0300

    Database config for heroku

--- src/database/index.js
+++ src/database/index.js
@@ -13,7 +13,7 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize(development);
+    this.connection = new Sequelize(process.env.DATABASE_URL || development);
 
     models
       .map((model) => model.init(this.connection))
