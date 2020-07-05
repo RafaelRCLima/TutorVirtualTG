d2dba165c9d02dd340390e5cbd24850e252a42f5
RafaelRCLima <rafael.lima19-92@hotmail.com>
2020-07-05 20:53:21 -0300

    heroku configuration

--- src/config/database.js
+++ src/config/database.js
@@ -17,16 +17,6 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_TEST,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at'
-    }
+    url: process.env.DATABASE_URL
   }
 }
